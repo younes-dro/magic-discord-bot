a6925070fe487b0b05a7669be40ae535e036ded1
Younes DRO <31171+younes-dro@users.noreply.github.com>
2025-09-30 22:04:00 +0100

    Refactor API routes for consistency and clarity

--- app/api/get-user-bot-commands/[userId]/route.jsx
+++ app/api/get-user-bot-commands/[userId]/route.jsx
@@ -1,42 +1,33 @@
-
-
-import prisma from '../../../libs/prismadb'
-import { NextResponse } from 'next/server'
-
+import prisma from "../../../libs/prismadb";
+import { NextResponse } from "next/server";
 
 export const GET = async (request, { params }) => {
-    
+  if (!params.userId) {
+    return new NextResponse("Missing user ID Field", { status: 400 });
+  }
 
-    if(!params.userId ) {
-        return new NextResponse('Missing user ID Field', { status: 400 })
+  try {
+    const userBotCommands = await prisma.Commands.findUnique({
+      where: {
+        userId: params.userId,
+      },
+    });
+
+    if (userBotCommands) {
+      console.log(userBotCommands);
+      return NextResponse.json(userBotCommands);
+    } else {
+      // console.log('none')
+      return NextResponse.json({
+        prefixCommand: "",
+        customCommands: {
+          getAllMemberships: "",
+          getHelp: "",
+        },
+      });
     }
-
-    try{
-
-        const userBotCommands = await prisma.Commands.findUnique({
-            where: {
-                userId : params.userId
-            }
-        });
-    
-        if(userBotCommands) {
-            console.log(userBotCommands)
-            return NextResponse.json(userBotCommands)
-            
-        } else{
-            // console.log('none')
-            return NextResponse.json( {
-                "prefixCommand": "",
-                "customCommands": {
-                  "getAllMemberships": "",
-                  "getHelp": ""
-                }
-              }
-              )
-        }
-
-    } catch(error){
-        console.error('Error fetching user bot commands:', error);
-        return new Response("Failed to fetch user bot commands", { status: 500 });
-    }
-}
+  } catch (error) {
+    console.error("Error fetching user bot commands:", error);
+    return new Response("Failed to fetch user bot commands", { status: 500 });
+  }
+};
--- app/api/register/route.jsx
+++ app/api/register/route.jsx
@@ -1,34 +1,34 @@
-import bcrypt from 'bcrypt'
-import prisma from '../../libs/prismadb'
-import { NextResponse } from 'next/server'
+import bcrypt from "bcrypt";
+import prisma from "../../libs/prismadb";
+import { NextResponse } from "next/server";
 
-export async function POST(request){
-    const body = await request.json();
-    const { name, email, password } = body;
+export async function POST(request) {
+  const body = await request.json();
+  const { name, email, password } = body;
 
-    if(!name || !email || !password) {
-        return new NextResponse('Missing Fields', { status: 400 })
-    }
+  if (!name || !email || !password) {
+    return new NextResponse("Missing Fields", { status: 400 });
+  }
 
-    const exist = await prisma.user.findUnique({
-        where: {
-            email
-        }
-    });
+  const exist = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
 
-    if(exist) {
-        throw new Error('Email already exists')
-    }
+  if (exist) {
+    throw new Error("Email already exists");
+  }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email,
-            hashedPassword
-        }
-    });
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      hashedPassword,
+    },
+  });
 
-    return NextResponse.json(user)
+  return NextResponse.json(user);
 }
